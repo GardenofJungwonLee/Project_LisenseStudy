8de8492e78fe2175fde092c014413f698f578356
qus0in <brillante31@naver.com>
2019-06-27 17:20:01 +0900

    0.0.2

--- js/page.js
+++ js/page.js
@@ -6,10 +6,12 @@ function prev() {
   page -= 1
   $("article").load("saj/"+ fname + "/" + page + ".html")
   }
+  else {alert("이전 페이지가 없습니다!")}
 };
 function next() {
   if (page !== pmax) {
   page += 1
   $("article").load("saj/"+ fname + "/" + page + ".html")
   }
+  else {alert("최대 페이지!")}
 };
